07749dbe5b1b1007daca64f1602c0d8fb66a8905
Justin Weber <jweber0169@Justins-MacBook-Pro.local>
2017-04-12 00:55:54 -0400

    Added base files and setup temporary html file structure. Setup sass folder structure and importing them to main.scss file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync').create()
 var postcss = require('gulp-postcss')
 var autoprefixer = require('autoprefixer')
 
@@ -14,7 +14,7 @@ gulp.task('scss', function() {
     autoprefixer({ browsers: ['last 2 versions']}),
   ];
   return gulp.src('./scss/*.scss')
-    pipe(sass())
+    .pipe(sass())
     .pipe(postcss(processors))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
